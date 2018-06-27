e9e795f6562a83de9d6c4c52e613d32e37884b57
Gabriel Dilly <gabrelsoueu@hotmail.com>
2018-06-27 18:33:43 -0300

    Saving user fields when signup with Linkedin

--- server/auth/linkedin/passport.js
+++ server/auth/linkedin/passport.js
@@ -1,7 +1,8 @@
 import passport from 'passport';
 import {Strategy as LinkedInStrategy} from 'passport-linkedin';
 import crypto from 'crypto';
-import {Position, Company} from '../../sqldb';
+import {Position, Company, Location, Industry} from '../../sqldb';
+import async from 'async';
 
 export function setup(User, config) {
   passport.use(new LinkedInStrategy({
@@ -16,34 +17,72 @@ export function setup(User, config) {
       ]
     },
     function (token, tokenSecret, profile, done) {
+      // console.log(profile);
       var email = profile.emails[0].value;
       var ImageURL = null;
       if(profile._json.pictureUrls) {
         ImageURL = profile._json.pictureUrls.values[0];
       }
-      User.find({
-        where: {
-          $or: [{LinkedinId: profile.id}, {Email: email}]
-        }
-      })
-        .then(user => {
+
+      async.waterfall([
+        // Finding user using his LinkedinId or his email
+        (next) => {
+          User.find({
+            where: {
+              $or: [{LinkedinId: profile.id}, {Email: email}]
+            }
+          })
+            .then(user => next(null, user))
+            .catch(err => next(err));
+        },
+        // Trying to find his industry
+        (user, next) => {
+          Industry.find({
+            where: {
+              $or: [
+                {Description: {$like: `%${profile._json.industry}%`}},
+                {PortugueseDescription: {$like: `%${profile._json.industry}%`}}
+              ]
+            }
+          })
+            .then(industry => next(null, user, industry))
+            .catch(err => next(null, user, null));
+        },
+        // Trying to save his location
+        (user, industry, next) => {
+          console.log(industry);
+          Location.findOrCreate({where: {LinkedinName: profile._json.location.name}})
+            .spread((location, created) => next(null, user, industry, location))
+            .catch(err => next(null, user, industry, null));
+        },
+        // Assigning user fields
+        (user, industry, location, next) => {
+          var industryId = null;
+          var locationId = null;
+          if(industry) {
+            industryId = industry.IndustryId;
+          }
+          if(location) {
+            locationId = location.LocationId;
+          }
           if(user) {
             // User just has his LinkedinId saved or has an account with the same email
-            user.name = profile.displayName;
             user.ImageURL = ImageURL;
             user.LinkedinId = profile.id;
             user.LinkedinProfileURL = profile._json.publicProfileUrl;
-            user.Headline = profile._json.headline || null;
-            user.Location = profile._json.location.name || null;
-            user.Industry = profile._json.industry || null;
             user.Summary = profile._json.summary || null;
             user.Specialties = profile._json.specialties || null;
-            saveUser(user, profile._json.positions.values || null, done);
+            // Fields that won't be changed for each login
+            //  user.name = profile.displayName;
+            //  user.Headline = profile._json.headline || null;
+            //  user.LocationId = profile._json.location.name || null;
+            //  user.IndustryId = profile._json.industry || null;
+            next(null, user, null);
           } else {
             // User is new
             crypto.randomBytes(20, function (err, buffer) {
               var token = buffer.toString('hex');
-              if (config.env === 'development'){
+              if(config.env === 'development') {
                 console.log(token);
               }
               if(!err) {
@@ -56,61 +95,118 @@ export function setup(User, config) {
                   LinkedinId: profile.id,
                   LinkedinProfileURL: profile._json.publicProfileUrl,
                   Headline: profile._json.headline || null,
-                  Location: profile._json.location.name || null,
-                  Industry: profile._json.industry || null,
+                  LocationId: locationId,
+                  IndustryId: industryId,
                   Summary: profile._json.summary || null,
                   Specialties: profile._json.specialties || null,
                   PersonTypeId: 1,
                   EmailVerified: 1,
                   ConfirmEmailToken: token
                 });
-                saveUser(user, profile._json.positions.values || null, done);
+                next(null, user, profile._json.positions.values);
               } else {
-                done(err);
+                next(err);
               }
-            }, err => done(err));
+            }, err => next(err));
           }
-        })
-        .catch(err => done(err));
-    }));
-}
+        },
+        // Saving user
+        (user, positions, next) => {
+          user.save()
+            .then(savedUser => {
 
-function saveUser(user, positions, done) {
-  user.save()
-    .then(savedUser => {
+              if(positions) {
+                async.eachSeries(positions, function (position, cb) {
+                  async.waterfall([
+                    // Trying to find company industry
+                    (next) => {
+                      Industry.find({
+                        where: {
+                          $or: [
+                            {Description: {$like: `%${position.company.industry}%`}},
+                            {PortugueseDescription: {$like: `%${position.company.industry}%`}}
+                          ]
+                        }
+                      })
+                        .then(industry => next(null, industry))
+                        .catch(err => next(null, null));
+                    },
+                    // Trying to save position location
+                    (industry, next) => {
+                      if(position.location) {
+                        Location.findOrCreate({where: {LinkedinName: position.location.name}})
+                          .spread((location, created) => next(null, industry, location))
+                          .catch(err => next(null, industry, null));
+                      }
+                    },
+                    // Trying to save company
+                    (industry, location, next) => {
+                      var industryId = null;
+                      if(industry) {
+                        industryId = industry.IndustryId;
+                      }
+                      Company.findOrCreate({
+                        where: {LinkedinId: position.company.id},
+                        defaults: {
+                          Name: position.company.name,
+                          Type: position.company.type,
+                          Size: position.company.size,
+                          IndustryId: industryId
+                        }
+                      })
+                        .spread((company, created) => next(null, location, company))
+                        .catch(err => next(null, location, null));
+                    },
+                    // Saving position
+                    (location, company, next) => {
+                      var locationId = null;
+                      if(location) {
+                        locationId = location.LocationId;
+                      }
 
-      if(positions) {
-        for(let position of positions) {
-          Company.findOrCreate({
-            where: {LinkedinId: position.company.id},
-            defaults: {
-              Name: position.company.name,
-              Type: position.company.type,
-              Industry: position.company.industry,
-              Size: position.company.size,
-            }
-          })
-            .then(result => {
-              var company = result[0];
-              Position.upsert({
-                PersonId: savedUser.PersonId,
-                CompanyId: company.CompanyId,
-                LinkedinId: position.id,
-                Title: position.title || null,
-                Summary: position.summary || null,
-                Location: (position.location ? position.location.name : null),
-                StartDateMonth: (position.startDate ? position.startDate.month : null),
-                StartDateYear: (position.startDate ? position.startDate.year : null),
-                EndDateMonth: (position.endDate ? position.endDate.month : null),
-                EndDateYear: (position.endDate ? position.endDate.year : null),
-                IsCurrent: 0
-              })
-                .catch(err => console.log(err));
+                      if(company) {
+                        Position.upsert({
+                          PersonId: savedUser.PersonId,
+                          CompanyId: company.CompanyId,
+                          LinkedinId: position.id,
+                          Title: position.title || null,
+                          Summary: position.summary || null,
+                          LocationId: locationId,
+                          StartDateMonth: (position.startDate ? position.startDate.month : null),
+                          StartDateYear: (position.startDate ? position.startDate.year : null),
+                          EndDateMonth: (position.endDate ? position.endDate.month : null),
+                          EndDateYear: (position.endDate ? position.endDate.year : null),
+                          IsCurrent: 0
+                        })
+                          .then(result => next(null, savedUser))
+                          .catch(err => console.log(err));
+                      } else {
+                        next(null, savedUser);
+                      }
+                    }
+                  ], function (err, result) {
+                    if(err) {
+                      cb(err);
+                    } else {
+                      cb(null, result);
+                    }
+                  });
+                }, next);
+
+              } else {
+                next(null, savedUser);
+              }
             })
-            .catch(err => console.log(err));
+            .catch(err => next(err));
         }
-      }
-      done(null, savedUser);
-    })
-    .catch(err => done(err));
+
+      ], function (err, result) {
+        if(err) {
+          done(err);
+        } else {
+          done(null, result);
+        }
+      });
+
+    }));
 }
