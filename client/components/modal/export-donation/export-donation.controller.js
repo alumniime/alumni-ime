219f242df1a46d056f2bc210938a86d779f1233f
Luiz Viana <luiz.viana000@gmail.com>
2019-08-27 14:31:32 -0300

    implemented excel export capabilities

--- client/components/modal/export-donation/export-donation.controller.js
+++ client/components/modal/export-donation/export-donation.controller.js
@@ -1,12 +1,17 @@
 import { runInThisContext } from "vm";
-
 'use strict';
 
 export default class ModalExportDonationController {
   submitted = false;
-  month;
-  year;
-  url;
+  donation={};
+  donator={};
+  cardbrand;
+  cardholdername;
+  cardLastDigits;
+  BoletoExpirationDate;
+  riskLevel;
+  status;
+  statusReason;
 
   /*@ngInject*/
   constructor(Modal, Upload, Newsletter, Util, $http, $filter) {
@@ -19,19 +24,42 @@ export default class ModalExportDonationController {
   }
 
   $onInit() {
-
+    this.$http.get('/api/donations')
+        .then(response=>{
+          this.donations=response.data;
+          console.log(this.donations);
+    });
   }
 
-  submitNews(form) {
+  export(form) {
     this.submitted = true;
-    console.log(form)
+    console.log(this.donation);
     
+    let output={};
+    /* starting from this data */
+var data = [
+  { name: "Barack Obama", pres: 44 },
+  { name: "Donald Trump", pres: 45 }
+];
+
+/* generate a worksheet */
+var ws = XLSX.utils.json_to_sheet(data);
+
+/* add to workbook */
+var wb = XLSX.utils.book_new();
+XLSX.utils.book_append_sheet(wb, ws, "Presidents");
+
+/* write workbook and force a download */
+XLSX.writeFile(wb, "sheetjs.xlsx");
 
     if(form.$valid){
 
       var loading = this.Modal.showLoading();
-
-      
+      Object.keys(this.donation).forEach(category => {
+        if (this.donation[category]===true) {
+          output.push
+        }
+      });
     }    
 
   }
