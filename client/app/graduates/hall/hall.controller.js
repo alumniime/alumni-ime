8f121c2c1423c172c01cfbcbea7e15bd5ee4673b
Luiz Viana <luiz.viana000@gmail.com>
2019-11-06 15:01:02 -0300

    fixed number of people in each category in donators hall

--- client/app/graduates/hall/hall.controller.js
+++ client/app/graduates/hall/hall.controller.js
@@ -19,6 +19,28 @@ export default class DonatorsHallController {
 
   $onInit() {
     var loading = this.Modal.showLoading();
+    this.getCurrentUser()
+      .then(user => {
+        this.user = user;
+        if (!this.user.PersonId) {
+          loading.close();
+          this.Modal.openLogin();
+          this.Modal.showAlert('Pesquisa indisponível', 'Apenas ex-alunos aprovados e logados podem realizar pesquisas.');
+        } else if (this.user.IsApproved && (this.user.personType.Description === 'FormerStudent' || this.user.personType.Description === 'FormerStudentAndProfessor') || this.user.role === 'admin' || this.user.IsSpecialUser) {
+
+          this.$http.get('/api/former_students/ranking')
+            .then(response => {
+              loading.close();
+              this.yearsRanking = response.data;
+              console.log(this.yearsRanking);
+              this.orderYears();
+            });
+
+        } else {
+          loading.close();
+          this.Modal.showAlert('Pesquisa indisponível', 'Apenas ex-alunos cadastrados e aprovados podem realizar pesquisas.');
+        }
+      });
     this.DonatorHall.load(false).then(()=>{
       loading.close();
       console.log(this.DonatorHall.list)
