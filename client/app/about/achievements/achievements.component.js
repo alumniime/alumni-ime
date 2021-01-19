1fb97e3341c0ae90ffc421b7f32da67a9d8a6db1
Davi <davihugomp@gmail.com>
2021-01-19 11:15:48 -0300

    Removed some code from achievements

--- client/app/about/achievements/achievements.component.js
+++ client/app/about/achievements/achievements.component.js
@@ -7,18 +7,10 @@ import routes from './achievements.routes';
 
 export class AchievementsController {
 
-  constructor(Auth, Modal, $http, $state) {
+  constructor() {
     'ngInject';
-
-    this.Auth = Auth;
-    this.$http = $http;
-    this.$state = $state;
-    this.Modal = Modal;
   }
 
-  openReports(year) {
-    this.Modal.openReports(year);
-  }
 }
   
 export default angular.module('alumniApp.achievements', [uiRouter])
