62bc2b2d78f44ff74c99bc215c2aa96de9da8741
Davi <davihugomp@gmail.com>
2021-02-07 10:17:20 -0300

    Removed unnecessary code

--- client/components/footer/footer.component.js
+++ client/components/footer/footer.component.js
@@ -1,17 +1,9 @@
 import angular from 'angular';
 
 export class FooterComponent {
-  name = '';
-  email = '';
-  message = '';
-  submitted = false;
 
-  constructor(Modal, $http, $uibModal) {
+  constructor() {
     'ngInject';
-
-    this.$http = $http;
-    this.Modal = Modal;
-    this.$uibModal = $uibModal;
   }
 
   $onInit() {
@@ -19,32 +11,6 @@ export class FooterComponent {
     this.year = date.getFullYear();
   }
 
-  sendContactEmail(form) {
-    this.submitted = true;
-
-    if(form.$valid){
-      var loading = this.Modal.showLoading();
-      this.$http.post('/api/users/contact', {
-        Name: this.name,
-        Email: this.email,
-        Message: this.message
-      })
-        .then(res => {
-          console.log(res);
-          loading.close();
-          this.Modal.showAlert('Email enviado!', 'Por favor, aguarde que lhe responderemos em breve.');
-          this.name = '';
-          this.email = '';
-          this.message = '';
-          this.submitted = false;
-        })
-        .catch(err => {
-          this.Modal.showAlert('Erro', 'Ocorreu um erro ao enviar o email, tente novamente.');
-          loading.close();
-          console.log(err);
-        });
-      }
-  }
 }
 
 export default angular.module('directives.footer', [])
