468a66a12c3da049935cc46a18d2cb7d063369ec
Davi <davihugomp@gmail.com>
2021-03-22 21:25:57 -0300

    New layout elements on projects list

--- client/app/projects/show/show.component.js
+++ client/app/projects/show/show.component.js
@@ -50,6 +50,14 @@ export class ShowController {
             }else{
               if(project.Year == this.Year && project.Semester == this.Semester){
                 project.show = true;
+
+
+                //ONLY FOR TEST
+                console.log(project);
+                project.DonationSum = project.CollectedPriceInCents;
+
+
+
               }else{
                 project.show = false;
               }
